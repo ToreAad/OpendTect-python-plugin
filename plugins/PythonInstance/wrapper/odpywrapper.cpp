60a46b6db7545b63ca4317676de5a2b156bbde4b
Tore Aadland <tore.aad@gmail.com>
2018-01-01 01:52:52 +0100

    Improved the python interface of the EMM() object.

--- plugins/PythonInstance/wrapper/odpywrapper.cpp
+++ plugins/PythonInstance/wrapper/odpywrapper.cpp
@@ -1,14 +1,60 @@
 #include "odpywrapper.h"
 #include "survinfo.h"
 #include "emmanager.h"
+#include "emobject.h"
 
 
 namespace EM
 {
-    class EMObject;
     class SurfaceIOData;
     class SurfaceIODataSelection;
 }
+
+class API_objectID
+{
+public:
+    API_objectID()
+    {
+
+    }
+
+    API_objectID(EM::ObjectID obj)
+	: m_obj{obj}
+    {
+
+    }
+    
+    int get_value()
+    {
+	return static_cast<int>(m_obj);
+    }
+
+    EM::ObjectID get()
+    {
+	return m_obj;
+    }
+
+private:
+    EM::ObjectID m_obj = -1;
+};
+
+class API_EMObject
+{
+public:
+    API_EMObject(){};
+
+    API_EMObject(EM::EMObject* obj)
+	:m_obj{obj}
+    {};
+
+    std::string name() const
+    {
+	return std::string(m_obj->name().getCStr());
+    }
+
+private:
+    EM::EMObject* m_obj = nullptr;
+};
 
 class Access_EMM
 {
@@ -21,26 +67,15 @@ public:
 	return EM::EMM().size();
     }
 
-    EM::ObjectID objectID(int idx)
+    API_objectID objectID(int idx)
     {
-	return EM::EMM().objectID(idx);
+	return API_objectID(EM::EMM().objectID(idx));
     }
 
-    bool objectExists(const EM::EMObject* emobj)
+    API_EMObject getObject(API_objectID idx)
     {
-	return EM::EMM().objectExists(emobj);
+	return API_EMObject(EM::EMM().getObject(idx.get()));
     }
-
-    bool loadIfNotFullyLoaded(const MultiID& mid, TaskRunner*  tr = nullptr)
-    {
-	return EM::EMM().loadIfNotFullyLoaded(mid, tr);
-    }
-
- //   EM::EMObject* getObject(const EM::ObjectID& eobj)
- //   {
-	//return EM::EMM().getObject(eobj);
- //   }
-
 };
 
 class Access_SI
@@ -65,10 +100,18 @@ bool Odpywrapper::register_class(boost::python::object& main_namespace)
 {
     namespace py = boost::python;
 
+    
+    py::class_<API_EMObject>("API_EMObject", py::init<>())
+	.def("__call__", &API_EMObject::name)
+	.def("name", &API_EMObject::name);
+    
+    py::class_<API_objectID>("API_objectID", py::init<int>())
+	.def("__call__", &API_objectID::get_value);
+
     main_namespace["EMM"] = py::class_<Access_EMM>("EM", py::init<>())
 	.def("size", &Access_EMM::size)
-	.def("objectID", &Access_EMM::objectID);
-	//.def("getObject", &Access_EMM::getObject);
+	.def("objectID", &Access_EMM::objectID)
+	.def("getObject", &Access_EMM::getObject);
 
     main_namespace["SI"] = py::class_<Access_SI>("SI", py::init<>())
 	.def("has2D", &Access_SI::has2D)
